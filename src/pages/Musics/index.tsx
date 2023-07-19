61718e0d6e163a4066d9457cc045c498c36eb41b
Gabriel Cabral Melo <contatogabemelo@gmail.com>
2023-07-19 11:07:07 -0300

    botão de voltar :)

--- src/pages/Musics/index.tsx
+++ src/pages/Musics/index.tsx
@@ -1,6 +1,6 @@
 import './Musics.css';
 import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import getMusics from '../../services/musicsAPI';
 import Loading from '../../components/Loading';
 import { AlbumType, SongType } from '../../types';
@@ -9,9 +9,10 @@ import MusicCard from '../../components/MusicCard';
 function Musics() {
   const location = useLocation();
   const { collectionId } = location.state ?? {};
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [musics, setMusics] = useState<SongType[]>();
   const [album, setAlbum] = useState<AlbumType>();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const findMusics = async () => {
@@ -24,12 +25,17 @@ function Musics() {
     findMusics();
   }, [collectionId]);
 
+  const handleBack = () => {
+    navigate(-1);
+  }
+
   if (isLoading) {
     return <Loading />;
   }
 
   return (
     <div className="musics-content">
+      <h3 className='voltar' onClick={ handleBack }>Voltar</h3>
       <div className="album-container">
         <img src={ album?.artworkUrl100 } alt={ `Album ${album?.collectionName}` } />
         <h1 data-testid="album-name">{album?.collectionName}</h1>
