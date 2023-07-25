a8cc6d898c6ff0d6cdd3fdd917f4a12e547a75a2
Gabriel Cabral Melo <contatogabemelo@gmail.com>
2023-07-25 12:30:25 -0300

    update Readme

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,14 @@ import EditProfile from './pages/EditProfile';
 function App() {
   return (
     <Routes>
-      <Route path="/TrybeTunes" element={ <Login /> } />
+      <Route path="/TrybeTunes/" element={ <Login /> } />
       <Route element={ <Layout /> }>
         <Route path="/TrybeTunes/search" element={ <Search /> } />
         <Route path="/TrybeTunes/album/:id" element={ <Musics /> } />
         <Route path="/TrybeTunes/favorites" element={ <Favorites /> } />
         <Route path="/TrybeTunes/profile" element={ <Profile /> } />
         <Route path="/TrybeTunes/profile/edit" element={ <EditProfile /> } />
-        <Route path="*" element={ <NotFound /> } />
+        <Route path="/TrybeTunes/*" element={ <NotFound /> } />
       </Route>
     </Routes>
   );
